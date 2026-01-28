5f98989b403b5bae244f4739ea2606ce0f228c68
kushagra gupta <kg7741365@gmail.com>
2026-01-28 13:18:07 +0530

    adds introductory questions patterns

--- src/components/InterviewLevel/ChildRecursiveAccordian.tsx
+++ src/components/InterviewLevel/ChildRecursiveAccordian.tsx
@@ -19,3 +19,9 @@ const ChildRecursiveAccordian = ({ node }) => {
 };
 
 export default ChildRecursiveAccordian;
+
+
+// microfrontend is a architectural design phoenomeon like microservives isme ham baat dete hai architecture ko small apps me sabka apna texh stack their own deployment confgis 
+// like for eg-amazon.com 
+// CART system
+// Product Listing 
