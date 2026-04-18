286ce42d9cd8439dbec18e3ba8fc1faa03428004
kushagra gupta <kg7741365@gmail.com>
2026-04-18 16:12:47 +0530

    adds code for flatten object in js and also usethrottle and debounce custom hooks incorporating react

--- src/components/InterviewLevel/js/RefComponent.jsx
+++ src/components/InterviewLevel/js/RefComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useCallback, useRef } from "react";
 
 const RefComponent = () => {
   const initialValue = 1;
@@ -8,3 +8,40 @@ const RefComponent = () => {
 };
 
 export default RefComponent;
+
+// custom hooks
+const useThrottle = (fn, delay) => {
+  const isThrottled = useRef(false);
+
+  const throttledFunction = useCallback(
+    (...args) => {
+      if (isThrottled.current) return;
+      fn(...args);
+      isThrottled.current = true;
+
+      setTimeout(() => {
+        isThrottled.current = false;
+      }, delay);
+    },
+    [fn, delay]
+  );
+
+  return throttledFunction;
+};
+
+const useDebounce = (fn, delay) => {
+  const timerRef = useRef(null);
+
+  const debouncedFunction = useCallback(
+    (...args) => {
+      clearInterval(timerRef.current);
+
+      timerRef.current = setTimeout(() => {
+        fn(...args);
+      }, delay);
+    },
+    [fn, delay]
+  );
+
+  return debouncedFunction
+};
