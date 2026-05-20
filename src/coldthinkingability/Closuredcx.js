8f780b1b014812182864d5ed174a8938bb078480
kushagra gupta <kg7741365@gmail.com>
2026-05-20 15:52:57 +0530

    adds code to prototype refrence and copy mechanism

--- src/coldthinkingability/Closuredcx.js
+++ src/coldthinkingability/Closuredcx.js
@@ -1,31 +1,81 @@
-console.log("A");
+// console.log("A");
 
-setTimeout(() => {
-  console.log("B");
+// setTimeout(() => {
+//   console.log("B");
 
-  Promise.resolve().then(() => {
-    console.log("C");
-  });
+//   Promise.resolve().then(() => {
+//     console.log("C");
+//   });
 
-  queueMicrotask(() => {
-    console.log("D");
-  });
-}, 0);
+//   queueMicrotask(() => {
+//     console.log("D");
+//   });
+// }, 0);
 
-Promise.resolve().then(() => {
-  console.log("E");
+// Promise.resolve().then(() => {
+//   console.log("E");
 
-  setTimeout(() => {
-    console.log("F");
-  }, 0);
-});
+//   setTimeout(() => {
+//     console.log("F");
+//   }, 0);
+// });
 
-queueMicrotask(() => {
-  console.log("G");
-});
+// queueMicrotask(() => {
+//   console.log("G");
+// });
 
-console.log("H");
+// console.log("H");
 
+// output - A,H,E,G,B,C,D,F
 
+// function outer() {
+//   let count = 0;
+//   return () => {
+//     count++;
+//     console.log(count);
+//   };
+// }
 
-output - A,H,E,G,B,C,D,F
+// const a = outer();
+// const b = outer();
+
+// a();
+// b();
+// a();
+
+// const obj = {
+//   name: "Kush",
+
+//   normal: function () {
+//     console.log(this.name);
+//   },
+
+//   arrow: () => {
+//     console.log(this.name);
+//   },
+// };
+
+// obj.normal();
+// obj.arrow();
+
+// const obj1= obj.normal
+// obj()
+
+// const obj2 = obj1.arrow
+// obj2()
+
+// Kush
+// undeinfed
+
+function A() {}
+
+A.prototype.x = 1;
+
+const obj1 = new A();
+
+A.prototype.x = 2;
+
+const obj2 = new A();
+
+console.log(obj1.x);
+console.log(obj2.x);
---
+++ src/coldthinkingability/objinterviewques.js
@@ -0,0 +1,34 @@
+// let nums = {
+//   a: 100,
+//   b: 200,
+//   title: "My nums",
+// };
+
+// function multiplyby2(nums) {
+//   for (let key in nums) {
+//     if (typeof nums[key] === "number") {
+//       nums[key] = nums[key] * 2;
+//     }
+//   }
+// }
+
+// multiplyby2(nums);
+// console.log(nums);
+
+// const a = {};
+// const b = { key: "b" };
+// const c = { key: "c" };
+
+// a[b] = 128;
+// a[c] = 456;
+
+// console.log(a[b]);
+
+let person1 = {
+  a: 20,
+  b: 10,
+};
+
+person1.b = 15;
+person1={}
+console.log(person1)
