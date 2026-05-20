32be36bb644fef95f771546f8d21b1c8dfff3675
kushagra gupta <kg7741365@gmail.com>
2026-05-21 01:33:05 +0530

    js prac

--- src/coldthinkingability/Closuredcx.js
+++ src/coldthinkingability/Closuredcx.js
@@ -67,15 +67,50 @@
 // Kush
 // undeinfed
 
-function A() {}
+// function A() {}
 
-A.prototype.x = 1;
+// A.prototype.x = 1;
 
-const obj1 = new A();
+// const obj1 = new A();
 
-A.prototype.x = 2;
+// A.prototype.x = 2;
 
-const obj2 = new A();
+// const obj2 = new A();
 
-console.log(obj1.x);
-console.log(obj2.x);
+// console.log(obj1.x);
+// console.log(obj2.x);
+
+let array1 = [1, 2, 3, 4];
+
+const arr2 = array1.reduce((item, acc) => {
+  return item + acc;
+});
+
+console.log(arr2);
+
+Array.prototype.myReduce = function (cb, initialValue) {
+  let acc = initialValue ? initialValue : this[0];
+  let start = initialValue ? 0 : 1;
+
+  for (let i = start; i < this.length; i++) {
+    acc = cb(acc, this[i], i, this);
+  }
+
+  return acc;
+};
+
+function throttle(fn, delay) {
+  let isThrottled = false;
+
+  return function (...args) {
+    if (isThrottled) return;
+
+    fn.apply(this, args);
+
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+    }, delay);
+  };
+}
